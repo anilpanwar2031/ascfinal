8b9d06395b30f908cd753a1c4955bc6e954e6ee6
SHRAY SALVI <80808215+shraysalvi@users.noreply.github.com>
2022-11-21 08:55:43 +0000

    commit finalized

--- app1/signals.py
+++ app1/signals.py
@@ -5,8 +5,7 @@ from app1.models import Organization, CustomUser
 
 @receiver(post_save, sender=Organization)
 def update_user(sender, instance, created, **kwargs):
-  if created:
-    owner_user = CustomUser.objects.get(id=instance.owner.id)
-    if not owner_user.org:
-      owner_user.org = Organization.objects.get(id=instance.id)
-      owner_user.save()
+  owner_user = CustomUser.objects.get(id=instance.owner.id)
+  if not owner_user.org:
+    owner_user.org = Organization.objects.get(id=instance.id)
+    owner_user.save()
